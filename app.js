b1203b68617dace1d029754e3cca239911f3ee80
동 진흠 <131103@mtr.local>
2017-08-24 14:53:28 +0900

    modify package.json

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ let session = require('express-session');
 let index = require('./routes/index');
 
 let app = express();
+let debug = require('debug')('sixstring:server');
+let http = require('http');
+
 
 // view engine setup
 app.engine('ejs', ejsLocals);
@@ -85,4 +88,61 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
-module.exports = app;
+let port = normalizePort(process.env.PORT || '80');
+app.set('port', port);
+
+let server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  let addr = server.address();
+  let bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
